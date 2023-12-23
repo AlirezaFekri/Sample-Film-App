525d25cb59963e7d6b7a411bf9f0ec2f449e4bf0
Alireza Fekri <alirezafekri.dev@gmail.com>
2023-12-23 23:02:26 +0330

    Add PropTypes tp StarRating

--- src/components/starRating.js
+++ src/components/starRating.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const containerStyle = {
     display: "flex",
@@ -9,14 +10,23 @@ const starsContainerStyles = {
     display: "flex",
 };
 
+StarRating.prototype = {
+    maxRating: PropTypes.nunmber,
+    size: PropTypes.number,
+    color: PropTypes.string,
+    message: PropTypes.array,
+    defaultRaing: PropTypes.number,
+    className: PropTypes.string,
+    onSetRating: PropTypes.func
+}
 
-function StarRating({ maxRating = 5, size = 48, starColor = "#fcc419", message = [], defaultRaing = 0, onSetRating }) {
+function StarRating({ maxRating = 5, size = 48, color = "#fcc419", message = [], defaultRaing = 0, className, onSetRating }) {
 
     const textStyle = {
         lineHeight: "1",
         margin: "0",
         fontSize: `${size / 1.2}px`,
-        color: starColor
+        color: color
     }
     const [rating, setRating] = useState(defaultRaing <= maxRating ? defaultRaing : 0)
     const [tempRating, setTempRating] = useState(0)
@@ -26,7 +36,7 @@ function StarRating({ maxRating = 5, size = 48, starColor = "#fcc419", message =
         onSetRating(rate)
     }
     return (
-        <div style={containerStyle}>
+        <div style={containerStyle} className={className}>
             <div style={starsContainerStyles}>
                 {
                     Array.from(
@@ -37,8 +47,8 @@ function StarRating({ maxRating = 5, size = 48, starColor = "#fcc419", message =
                                 onHoverIn={(() => setTempRating(i + 1))}
                                 onHoverOut={(() => setTempRating(0))}
                                 onRate={() => HandleRating(i + 1)}
-                                starSize={size}
-                                starColor={starColor}
+                                size={size}
+                                color={color}
                             />
                         ))
                 }
@@ -48,17 +58,17 @@ function StarRating({ maxRating = 5, size = 48, starColor = "#fcc419", message =
     )
 }
 
-function Star({ onRate, full, onHoverIn, onHoverOut, starSize, starColor }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, size, color }) {
     const starStyle = {
-        width: `${starSize}px`,
-        height: `${starSize}px`,
+        width: `${size}px`,
+        height: `${size}px`,
         display: "block",
         cursor: "pointer"
     }
 
     return (
         <span onClick={onRate} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut} role='button' style={starStyle}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill={full ? starColor : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke={starColor} >
+            <svg xmlns="http://www.w3.org/2000/svg" fill={full ? color : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke={color} >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
             </svg>
         </span>
